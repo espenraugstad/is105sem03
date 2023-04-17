21048c00622b509e917ef27668a6cd81b13e9fa3
espenraugstad <espen.raugstad@gmail.com>
2023-04-17 14:33:28 +0200

    Testene passerer

--- mycrypt/mycrypt_test.go
+++ mycrypt/mycrypt_test.go
@@ -11,14 +11,15 @@ func TestKrypter(t *testing.T) {
 
 	type test struct {
 		inputMessage []rune
-                chiffer int
-		want  []rune
+		chiffer      int
+		want         []rune
 	}
 	tests := []test{
 		{inputMessage: []rune("w"), chiffer: 4, want: []rune("æ")},
 		{inputMessage: []rune("0"), chiffer: 4, want: []rune("4")},
-		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("dnizmocdd7;484c5: 47 6466d45b94c.")},
-                {inputMessage: []rune("dnizmocdd7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
+		// Oppdaterer her sånn at det passer med det oppdaterte alfabetet.
+		{inputMessage: []rune("Kjevik;SN39040;18.03.2022 01:50;6"), chiffer: 4, want: []rune("OnizmocWR7;484c5: 47 6466d45b94c.")},
+		{inputMessage: []rune("OnizmocWR7;484c5: 47 6466d45b94c."), chiffer: len(ALF_SEM03) - 4, want: []rune("Kjevik;SN39040;18.03.2022 01:50;6")},
 	}
 
 	for _, tc := range tests {
